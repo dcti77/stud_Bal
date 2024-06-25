d6199bf7296d055e5ce49c187c7a622b2ca1dcc3
Vitali.Kondrus <dcti77@gmail.com>
2024-06-25 20:48:08 +0300

    list comprehemsion lesson added 2

--- list comprehension.py
+++ list comprehension.py
@@ -1,2 +1,11 @@
-l = [x + ' ggg' for x in 'gradus']
-print(l)
+# l = [x + ' ggg' for x in 'gradus']
+# print(l)
+
+
+x = input()
+
+lst1 = [float(a) for a in x.split()]
+lst = [abs(a) for a in lst1]
+
+print(lst)
+
