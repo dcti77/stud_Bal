5e4864abd94f9dfef131887f6bc7624f932eff61
Vitali.Kondrus <dcti77@gmail.com>
2024-06-26 11:48:12 +0300

    list comprehemsion lesson added 3

--- list comprehension.py
+++ list comprehension.py
@@ -1,11 +1,32 @@
 # l = [x + ' ggg' for x in 'gradus']
 # print(l)
 
+#
+# x = input()
+#
+# lst1 = [float(a) for a in x.split()]
+# lst = [abs(a) for a in lst1]
+#
+# print(lst)
 
-x = input()
+# t = ['qwe', 'dqwd']
+# e = [x for x in t]
+# print(e)
+# print(id(t))
+# print(id(e))
+# print(type(t))
+# print(type(e))
 
-lst1 = [float(a) for a in x.split()]
-lst = [abs(a) for a in lst1]
 
-print(lst)
+lst_in = [[1, 2, 3, 4],
+          [5, 6, 7, 8],
+          [9, 8, 7, 6],
+          [5, 4, 3, 2]]
 
+res = [x
+       for row in lst_in
+       for x in row]
+
+res.reverse()
+
+print(*res)
